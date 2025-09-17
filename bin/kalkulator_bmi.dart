ddaaed32c10148addbb82be066bbc7ce2dbaaced
rohmanalka <rohmanalka06@gmail.com>
2025-09-17 14:50:27 +0700

    add bmi calculation

--- bin/kalkulator_bmi.dart
+++ bin/kalkulator_bmi.dart
@@ -9,3 +9,31 @@ void main() {
   // Tampilkan riwayat menggunakan perulangan
   tampilkanRiwayat(riwayat);
 }
+
+void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
+  // Konversi tinggi dari cm ke m
+  double tinggiM = tinggiCm / 100;
+
+  // Hitung BMI
+  double bmi = beratKg / (tinggiM * tinggiM);
+
+  // Tentukan kategori menggunakan percabangan
+  String kategori;
+  if (bmi < 18.5) {
+    kategori = "Kurus";
+  } else if (bmi < 25) {
+    kategori = "Normal";
+  } else if (bmi < 30) {
+    kategori = "Gemuk";
+  } else {
+    kategori = "Obesitas";
+  }
+
+  // Simpan hasil ke riwayat
+  riwayat.add({
+    'tinggi': tinggiCm,
+    'berat': beratKg,
+    'bmi': bmi,
+    'kategori': kategori
+  });
+}
