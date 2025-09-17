dde3d5e2a9d09acb5e17a614484c4864397ecd11
rohmanalka <rohmanalka06@gmail.com>
2025-09-17 14:49:47 +0700

    add input validation

---
+++ bin/kalkulator_bmi.dart
@@ -0,0 +1,11 @@
+void main() {
+  List<Map<String, dynamic>> riwayat = []; // List untuk menyimpan riwayat
+
+  // Simulasi beberapa input
+  hitungBMI(170, 65, riwayat);
+  hitungBMI(165, 75, riwayat);
+  hitungBMI(180, 85, riwayat);
+
+  // Tampilkan riwayat menggunakan perulangan
+  tampilkanRiwayat(riwayat);
+}
